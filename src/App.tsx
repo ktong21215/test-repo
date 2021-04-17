4d35eba0d68e608b0ad05d67749ba37740f974de
Michael Tong <80925391+ktong21215@users.noreply.github.com>
2021-04-16 17:27:49 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { ReactElement } from 'react';
+import './App.css';
+
+const App = (): ReactElement => {
+  return (
+    <div className="App">
+      Hello World!
+    </div>
+  );
+}
+
+export default App;
